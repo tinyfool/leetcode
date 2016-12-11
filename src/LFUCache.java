63cc92c20bb30b7cf5a670765350451d156a08d1
HaoPeiqiang <tinyfool@gmail.com>
2016-12-11 09:06:10 +0800

    Add LFU question

---
+++ src/LFUCache.java
@@ -0,0 +1,28 @@
+/**
+ * Created by haopeiqiang on 2016/12/11.
+ */
+// 460. LFU Cache
+// https://leetcode.com/problems/lfu-cache/
+    
+public class LFUCache {
+
+    public LFUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+
+        return 0;
+    }
+
+    public void set(int key, int value) {
+
+    }
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.set(key,value);
+ */
