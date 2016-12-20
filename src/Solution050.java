9abc22fec158a13803f0d9a19412f9fc81d9b920
HaoPeiqiang <tinyfool@gmail.com>
2016-12-20 17:53:48 +0800

    pass 050

---
+++ src/Solution050.java
@@ -0,0 +1,43 @@
+/**
+ * Created by haopeiqiang on 2016/12/20.
+ */
+public class Solution050 {
+
+    public double myPow(double x, int n) {
+
+        double result = 1.0;
+
+        int m = n;
+        if (m == 0)
+            return 1.0;
+        if(n==-2147483648)
+            m = 2147483647;
+
+        if (m < 0)
+            m = - m;
+        double ox = x;
+        while( m >0) {
+
+            if(((int)m & 1) ==1 ) {
+                result *= x;
+            }
+
+            x *= x;
+            m >>= 1;
+        }
+        if(n==-2147483648)
+            result *= ox;
+
+        if (n < 0)
+            return 1/result;
+        else
+            return result;
+    }
+
+    public static void main(String[] argv) {
+
+        Solution050 s = new Solution050();
+        System.out.print(s.myPow(-1
+                ,-2147483648));
+    }
+}
