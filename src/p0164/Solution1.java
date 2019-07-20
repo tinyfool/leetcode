2ec7d293b0cbb81e8f1c3012259799a70ec68822
HaoPeiqiang <tinyfool@gmail.com>
2019-07-20 17:39:15 +0800

    fixed 164 Runtime: 2 ms, faster than 98.90% of Java online submissions for Maximum Gap.

---
+++ src/p0164/Solution1.java
@@ -0,0 +1,31 @@
+package p0164;
+
+import java.util.Arrays;
+
+//Runtime: 2 ms, faster than 98.90% of Java online submissions for Maximum Gap.
+
+public class Solution1 {
+
+    public int maxiumGap(int[] nums) {
+        
+        if (nums.length<2)
+            return 0;
+        Arrays.sort(nums);
+        int diff;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i<nums.length-1; i++) {
+
+            diff = nums[i+1]-nums[i];
+            if(max<diff)
+                max = diff;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        int[] input = new int[]{3,6,9,1};
+        Solution1 s1 = new Solution1();
+        System.out.println(s1.maxiumGap(input));
+    }
+}
