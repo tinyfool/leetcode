d61cb6a54ffab9525fdd8554ed048ca9cb52ef0c
HaoPeiqiang <Tinyfool@gmail.com>
2019-08-04 10:59:08 +0800

    add helper to compare two tree

--- src/helper/TreeNode.java
+++ src/helper/TreeNode.java
@@ -1,4 +1,5 @@
 package helper;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TreeNode {
     public int val;
@@ -6,6 +7,17 @@ public class TreeNode {
     public TreeNode right;
     public TreeNode(int x) {
         val = x;
+    }
+
+    public static boolean sameTree(TreeNode p,TreeNode q) {
+
+        if(p==null&&q==null)
+            return true;
+        if(p==null||q==null)
+            return false;
+        if(p.val!=q.val)
+            return false;
+        return sameTree(p.left,q.left) && sameTree(p.right,q.right);
     }
 }
 
